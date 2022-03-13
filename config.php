001bcd3971bc7c3324fa84e3ca65e4070d9a987a
chaitanya rai <57841263+chaitanyarai19@users.noreply.github.com>
2022-03-13 10:23:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   define('DB_SERVER', '144.24.112.64');
+   define('DB_USERNAME', 'crime_reporting');
+   define('DB_PASSWORD', 'Safetyfirst@12345');
    define('DB_DATABASE', 'crime_reporting');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
